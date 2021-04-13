9587b2b66ceb800cfdc32235577c1c036e4f322a
osouzaTI <o.souzati@gmail.com>
2021-04-13 01:30:25 -0400

    utilizando componente p_xor no subtrator

--- components/subtrator/subtrator.vhd
+++ components/subtrator/subtrator.vhd
@@ -4,12 +4,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity subtrator is
-	port(
-	
-			in_a : in std_logic_vector (7 downto 0); 
-			in_b : in std_logic_vector (7 downto 0);
-			cout : out std_logic; 
-			s: out std_logic_vector(7 downto 0)
+	port(	
+		in_a : in std_logic_vector (7 downto 0); 
+		in_b : in std_logic_vector (7 downto 0);
+		cout : out std_logic; 
+		s: out std_logic_vector(7 downto 0)
 	);
 end entity;
 
@@ -28,9 +27,9 @@ architecture logic of subtrator is
 
 	component p_xor is
 		 port (
-			  in_port_a : in std_logic;
-			  in_port_b : in std_logic;
-			  out_port : out std_logic
+			in_port_a : in std_logic;
+			in_port_b : in std_logic;
+			out_port : out std_logic
 		 );
 	end component p_xor;
 	
@@ -38,13 +37,16 @@ architecture logic of subtrator is
 	
 	begin
 	
-		comp2 <= "11111111" xor in_b; 
+		-- comp2 <= "11111111" xor in_b; 
 		
-		somador1 : somador8bits
-		port map(
-					
-					in_a, comp2, '1', cout, s 
-		);
-		
+		xor0: p_xor port map('1', in_b(7), comp2(7));
+		xor1: p_xor port map('1', in_b(6), comp2(6));
+		xor2: p_xor port map('1', in_b(5), comp2(5));
+		xor3: p_xor port map('1', in_b(4), comp2(4));
+		xor4: p_xor port map('1', in_b(3), comp2(3));
+		xor5: p_xor port map('1', in_b(2), comp2(2));
+		xor6: p_xor port map('1', in_b(1), comp2(1));
+		xor7: p_xor port map('1', in_b(0), comp2(0));
+		somador1 : somador8bits port map(in_a, comp2, '1', cout, s);		
 	  
 end logic; 
